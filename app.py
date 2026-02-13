1906caf51f544f65227e6052a254ad857b65c4ca
clemens-pixner <clemenspixner2.0@gmail.com>
2026-02-13 19:13:52 +0100

    app changes

--- app.py
+++ app.py
@@ -5,9 +5,13 @@ import streamlit as st
 from database import init_database, insert_aircraft
 from scraper import scrape_page
 
+df = pd.read_sql_table()
+#aircrafts, run_time = scrape_page()
 
-
-init_database()
-aircrafts, run_time = scrape_page() 
-insert_aircraft(aircrafts, run_time)
-
+st.title("Helicopter market-dashboard")
+st.dataframe(data=df)
+with st.sidebar:
+    if st.button("Scrape"):
+        #init_database()
+        #insert_aircraft(aircrafts, run_time)
+        st.write("Hello")
