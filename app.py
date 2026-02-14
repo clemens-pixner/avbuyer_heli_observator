b94cd559c1bfb61c52e62b9c66120c789acbfffe
clemens-pixner <clemenspixner2.0@gmail.com>
2026-02-14 12:47:37 +0100

    starting to build app.py

--- app.py
+++ app.py
@@ -1,17 +1,45 @@
 import pandas as pd
-import sqlite3
 import streamlit as st 
 
-from database import init_database, insert_aircraft
+from database import db_conn, init_database, insert_aircraft
 from scraper import scrape_page
 
-df = pd.read_sql_table()
-#aircrafts, run_time = scrape_page()
+def dataframe(df):
+    show_df = df.drop(columns=["aircraft_id", "timestamp", "active"])
+
+    return st.dataframe(
+            show_df,
+            column_config={
+                "url": st.column_config.LinkColumn(
+                    "URL",
+                    display_text="url" 
+                )
+            },
+            width="stretch"
+        )
+
+init_database()
+
+with db_conn() as conn:
+    df = pd.read_sql_query("SELECT * FROM aircraft WHERE active = 1", conn)
 
 st.title("Helicopter market-dashboard")
-st.dataframe(data=df)
-with st.sidebar:
-    if st.button("Scrape"):
-        #init_database()
-        #insert_aircraft(aircrafts, run_time)
-        st.write("Hello")
+
+col_1, col_2 = st.columns(2, border=True)
+
+with col_1:
+    st.markdown(":blue-background[Chart]")
+    st.bar_chart()
+
+with col_2:
+    st.markdown(":blue-background[Raw data]")
+    dataframe(df)
+
+with st.container(border=True, width="stretch", horizontal=True):
+        if st.button("Scrape"):
+            aircrafts, run_time = scrape_page()
+            insert_aircraft(aircrafts, run_time)
+            st.rerun()
+
+        st.space("stretch")
+        st.markdown(":grey-background[Last scrape:]")
--- database.py
+++ database.py
@@ -1,55 +1,56 @@
 import sqlite3
 
-def init_database():
-    conn = sqlite3.connect("aircraft.db")
-    cur = conn.cursor()
-    
-    cur.execute("""
-    CREATE TABLE IF NOT EXISTS aircraft (
-    aircraft_id INTEGER PRIMARY KEY AUTOINCREMENT,
-    url TEXT UNIQUE NOT NULL,
-    brand TEXT NOT NULL,
-    model TEXT NOT NULL,
-    eur_price REAL,
-    foreign_price REAL,
-    currency TEXT,
-    timestamp TEXT NOT NULL,
-    active INTEGER NOT NULL         
-    )
-    """)
+def db_conn():
+    return sqlite3.connect("aircraft.db")
 
-    conn.commit()
-    conn.close()
+def init_database():
+    with db_conn() as conn:
+        cur = conn.cursor()
+        
+        cur.execute("""
+        CREATE TABLE IF NOT EXISTS aircraft (
+        aircraft_id INTEGER PRIMARY KEY AUTOINCREMENT,
+        url TEXT UNIQUE NOT NULL,
+        brand TEXT NOT NULL,
+        model TEXT NOT NULL,
+        eur_price REAL,
+        foreign_price REAL,
+        currency TEXT,
+        timestamp TEXT NOT NULL,
+        active INTEGER NOT NULL         
+        )
+        """)
+
+        conn.commit()
 
 def insert_aircraft(aircrafts, run_time):
 
     if not aircrafts:
         return
     
-    conn = sqlite3.connect("aircraft.db")
-    cur = conn.cursor()
+    with db_conn() as conn:
+        cur = conn.cursor()
 
-    cur.executemany("""
-    INSERT INTO aircraft (url, brand, model, eur_price, foreign_price, currency, timestamp, active)
-    VALUES (?, ?, ?, ?, ?, ?, ?, ?)
-    ON CONFLICT(url) DO UPDATE SET
-            brand=excluded.brand,
-            model=excluded.model,
-            eur_price=excluded.eur_price,
-            foreign_price=excluded.foreign_price,
-            currency=excluded.currency,
-            timestamp=excluded.timestamp,
-            active=1    
-    """, [
-        (a["url"], a["brand"], a["model"], a["eur_price"], a["foreign_price"], a["currency"], a["timestamp"], run_time)
-          for a in aircrafts
-    ])
-    
-    cur.execute("""
-    UPDATE aircraft
-    SET active = 0 
-    WHERE timestamp != ?
-    """, (run_time,))
+        cur.executemany("""
+        INSERT INTO aircraft (url, brand, model, eur_price, foreign_price, currency, timestamp, active)
+        VALUES (?, ?, ?, ?, ?, ?, ?, ?)
+        ON CONFLICT(url) DO UPDATE SET
+                brand=excluded.brand,
+                model=excluded.model,
+                eur_price=excluded.eur_price,
+                foreign_price=excluded.foreign_price,
+                currency=excluded.currency,
+                timestamp=excluded.timestamp,
+                active=1    
+        """, [
+            (a["url"], a["brand"], a["model"], a["eur_price"], a["foreign_price"], a["currency"], run_time, a["active"])
+            for a in aircrafts
+        ])
+        
+        cur.execute("""
+        UPDATE aircraft
+        SET active = 0 
+        WHERE timestamp != ?
+        """, (run_time,))
 
-    conn.commit()
-    conn.close()
+        conn.commit()
